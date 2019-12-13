32dae6acdd94b0aa7bc8cfaf6af94f7b3d35b36d
Student <example@example.com>
2019-12-13 10:56:16 -0800

    research on how to set up project

--- bakery.cs
+++ bakery.cs
@@ -8,7 +8,19 @@ namespace PierresBakery
     {
         public static void Main()
        {
+           Console.WriteLine("Welcome to Pierre's Bakery! Fresh out the oven, we have loaves of bread and various pasteries for up to $5.00 USD each plus sales tax.");
+
+           Bakery = new Bakery();
+           Console.WriteLine("How many loaves of bread would you like to purchase?");
+           int customerBakery = int.Parse(Console.ReadLine());
+           int balanceBakery = Bakery.BakeryBalance(customerBakery);
            
+           Pastery = new Pastery();
+           Console.WriteLine("How many pasteries woud you like to purchase?");
+           int customerPastery = int.Parse(Console.ReadLine());
+           int balancePastery = Pastery.PasteryBalance(customerPastery);
+
+           Console.WriteLine("The total of your balance due is: $\" + totalBalance + \". Thank you very much for supporting our business!");
        }
     }
 }
---
+++ pastery.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace PierresPastery
+{
+    public class Pastery
+    {
+        public static int PasteryBalance(int itemsPastery)
+        {
+            int pricePastery = Cost * itemsPastery;
+            if (itemsPastery )
+        }
+
+    }
+}
