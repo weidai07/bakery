c48af88d700076a49e99bf7d2d797f17a09c6914
Student <example@example.com>
2019-12-13 16:48:57 -0800

    finalize project

--- client.cs
+++ client.cs
@@ -3,23 +3,22 @@ using System.Threading;
 using System.Collections.Generic;
 using Project7;
 
-public class PierresPastery
+public class Dough
     {
         public static void Main()
        {
            Console.WriteLine("Welcome to Pierre's Bakery! Fresh baked hourly, we offer loaves of bread and various pasteries for up to $5.00 USD each plus sales tax.");
 
-           Bakery pBakery = new Bakery();
+           Bakery bakery = new Bakery();
            Console.WriteLine("How many loaves of bread would you like to purchase?");
            int itemsBakery = int.Parse(Console.ReadLine());
-           int balanceBakery = pBakery.BakeryBalance(itemsBakery);
+           int balanceBakery = bakery.BakeryBalance(itemsBakery);
            
-           Pastery pPastery = new Pastery();
            Console.WriteLine("How many pasteries woud you like to purchase?");
            int itemsPastery = int.Parse(Console.ReadLine());
-           int balancePastery = pPastery.PasteryBalance(itemsPastery);
+           int balancePastery = Pastery.PasteryBalance(itemsPastery);
 
            int totalBalance = balancePastery + balanceBakery;
-           Console.WriteLine("The total of your balance due is: $" + totalBalance + ". Thank you very much for supporting our business!");
+           Console.WriteLine("The total of your balance due is: $" + totalBalance + ".00 USD. Thank you very much for supporting our business!");
        } 
     }
