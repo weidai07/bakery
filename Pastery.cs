abe0915df8b3b349d1dc3ae568e46f9f2521cf21
Student <example@example.com>
2020-01-13 14:45:18 -0800

    minor error fix

--- Pastery.cs
+++ Pastery.cs
@@ -35,37 +35,3 @@ namespace Bakery
         }
     }
 }
-
-
-
-
-// namespace Project7
-// {
-
-//     public class Pastery
-//     {
-//         private int ItemsPastery { get; set; }
-
-//         public static int PasteryBalance(int itemsPastery)
-//         {   
-//             ItemsPastery = itemsPastery;
-//             int Cost = 2;
-//             int pricePastery = Cost * itemsPastery;
-//             int pasteryDiscount = itemsPastery / 3;
-//             int balancePastery = pricePastery - pasteryDiscount;
-//             if (itemsPastery >= 3)
-//             { 
-//             return balancePastery; 
-//             }   
-//             else if (itemsPastery < 3)
-//             { 
-//             return pricePastery; 
-//             }
-//         int PasteryBalance = balancePastery + pricePastery;
-//         return PasteryBalance;
-//         }
-
-//     }
-// }
-
-
--- main.cs
+++ main.cs
@@ -17,32 +17,10 @@ public class Dough
         Console.WriteLine("How many pasteries woud you like to purchase?");
         int itemsPastery = int.Parse(Console.ReadLine());
         Pastery pasteryOrder = new Pastery(itemsPastery);
-        int finalPasteryCosts = pasteryOrder.finalPasteryCosts();
+        int finalPasteryCosts = pasteryOrder.PasteryCosts();
 
         int totalBalance = finalBreadCosts + finalPasteryCosts;
 
-        Console.WriteLine("The total of your balance due is: $" + totalBalance + ".00 USD. Thank you very much for supporting our business!");    
+        Console.WriteLine("The total of your balance due is: $" + totalBalance + " USD. Thank you very much for supporting our business!");    
     } 
 } 
-
-
-
-
-
-
-
-        // Console.WriteLine("Welcome to Pierre's Bakery! Fresh baked hourly, we offer loaves of bread and various pasteries for up to $5.00 USD each plus sales tax.");
-
-        // Bakery bakery = new Bakery();
-        // Console.WriteLine("How many loaves of bread would you like to purchase?");
-        // int itemsBakery = int.Parse(Console.ReadLine());
-        // int balanceBakery = bakery.BakeryBalance(itemsBakery);
-        
-        // Console.WriteLine("How many pasteries woud you like to purchase?");
-        // int itemsPastery = int.Parse(Console.ReadLine());
-        // int balancePastery = Pastery.PasteryBalance(itemsPastery);
-
-        // float totalBalance = (balancePastery + balanceBakery) * 10.10;
-        // Console.WriteLine("The total of your balance due is: $" + totalBalance + ".00 USD. Thank you very much for supporting our business!");
-
-
